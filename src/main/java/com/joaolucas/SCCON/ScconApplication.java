5107d94ba23e44c966b7131d5afa1b511476c386
VasconcelosJoao <joao.brasilpv@gmail.com>
2025-01-22 00:04:12 -0300

    Initial Commit

---
+++ src/main/java/com/joaolucas/SCCON/ScconApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.SCCON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolucas/SCCON/ScconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.SCCON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
